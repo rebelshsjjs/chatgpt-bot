f060fcbb46529dd84f5d28736b8960f072a3e404
rebelshsjjs <yuhgfgjjjggjjj@gmail.com>
2025-02-02 09:11:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -4,13 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = environ.get("API_ID" , "2099007")
-API_HASH = environ.get("API_HASH" , "b1b3d7b3b1b3d7b3b1b3d7b3b1b3d7b3")
+API_ID = environ.get("API_ID" , "26162072")
+API_HASH = environ.get("API_HASH" , "ba25181c01b50d945748801b6c8b6ecc")
 BOT_TOKEN = environ.get("BOT_TOKEN" , "2099007:AAH1b3d7b3b1b3d7b3b1b3d7b3b1b3d7b3")
-ADMIN = int(environ.get("ADMIN" , "1847899007"))
+ADMIN = int(environ.get("ADMIN" , "6717382350"))
 CHAT_GROUP = int(environ.get("CHAT_GROUP", "-100"))
 LOG_CHANNEL = environ.get("LOG_CHANNEL", "-100")
-MONGO_URL = environ.get("MONGO_URL" , "mongodb://localhost:27017")
+MONGO_URL = environ.get("MONGO_URL" , "mongodb+srv://rebelbotz22:vNcEEoNvSQ33d44K@cluster0.oj1hu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 AUTH_CHANNEL = int(
     environ.get("AUTH_CHANNEL", "-1001734958816")
 )
